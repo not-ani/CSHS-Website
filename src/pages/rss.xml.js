52e67b6eff5695e41d456133af768da6ffaf5fa1
xiften <108333567+not-ani@users.noreply.github.com>
2024-01-05 11:47:35 -0700

    added base button components

--- src/pages/rss.xml.js
+++
@@ -1,9 +0,0 @@
-import rss from "@astrojs/rss";
-
-export async function GET(context) {
-  return rss({
-    title: "Creek CSHS",
-    description: "Cherry Creek Computer Science Honor Society",
-    site: site.context,
-  });
-}
