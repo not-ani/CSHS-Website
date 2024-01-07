f929a94752e94691c3530cb30119c734dba0ec55
xiften <108333567+not-ani@users.noreply.github.com>
2024-01-06 22:37:08 -0700

    fix: imporved FAQ questions and answers

--- src/components/ApplyFAQ.tsx
+++ src/components/ApplyFAQ.tsx
@@ -9,23 +9,27 @@ import {
 const FAQAccordion = () => {
   const faqData = [
     {
-      question: "Can I Still Apply if I don't meet the requirements?",
+      question:
+        "What if I don't meet all the eligibility criteria? Can I still apply for the CSHS?",
       answer:
-        "Yes. When you fill your application, if you select 'no' on the question, you will be asked to fill out a waiver form. After you finish that you can apply.",
+        "Absolutely. If you find that you don't meet certain criteria, you can still apply. When completing your application, simply indicate 'no' for the respective question, and you'll be prompted to fill out a waiver form. Complete this, and you can proceed with your application.",
     },
     {
-      question: "How are CSHS applications like NHS applications?",
+      question:
+        "How does the CSHS application process differ from the NHS application process?",
       answer:
-        "NHS does applications at the end of the year and accept the subjsequent year. We allow you to apply year round and accept you on a rolling basis.",
+        "The primary difference lies in the timing. NHS typically conducts its application process at the end of the academic year and confirms acceptance for the following year. In contrast, CSHS accepts applications throughout the year and processes them on a rolling basis, allowing for more flexibility in application and acceptance times.",
     },
     {
-      question: "When will I hear back?/ when should I apply?",
+      question:
+        "When can I expect to hear back after submitting my application, and what is the best time to apply?",
       answer:
-        "We allow you to apply year round and accept you on a rolling basis. Each batch of applications will be reviewed every one-two weeks. You will hear back from us at that time",
+        "We review applications in batches every one to two weeks throughout the year, so you can apply at any time that's convenient for you. After each review period, we will notify you about your application status.",
     },
     {
-      question: "How do I know if I got accepted?",
-      answer: "You will receive an email from us with further instructions.",
+      question: "How will I be informed if I am accepted into the CSHS?",
+      answer:
+        "Upon acceptance, you'll receive an email from us with all the necessary details and further instructions regarding your membership and next steps.",
     },
   ];
 
